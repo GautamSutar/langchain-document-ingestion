cdd309915f98594bd5fa592d2d4ed2930345afcd
Gautam Sutar <gautamsutar.in@gmail.com>
2026-05-02 14:52:07 +0530

    text_aplitter working

--- lenth_based.py
+++ lenth_based.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 
 text= """Technology has become an integral part of modern life, transforming the way we communicate, work, and solve problems. From artificial intelligence and cloud computing to smartphones and the Internet of Things, technological advancements are continuously improving efficiency and connectivity across the globe. Businesses leverage data analytics and automation to make smarter decisions, while individuals benefit from instant access to information and digital services. At the same time, rapid innovation also brings challenges such as data privacy, cybersecurity, and ethical concerns around AI. As technology continues to evolve, its responsible use will play a crucial role in shaping a more innovative, inclusive, and sustainable future.
 """
@@ -6,7 +6,7 @@ text= """Technology has become an integral part of modern life, transforming the
 splitter = CharacterTextSplitter(
     chunk_size=20,
     chunk_overlap=0,
-    seperator=''
+    separator=''
     )    
 result = splitter.split_text(text)
 print(result)
