b1aa98fa6f7e67fca99da975f8c68a4c5180b596
Gautam Sutar <gautamsutar.in@gmail.com>
2026-05-02 13:03:17 +0530

    webbase_loader working fine and agent talking

--- webbase_loader.py
+++ webbase_loader.py
@@ -1,7 +1,28 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate 
+from langchain_text_splitters import RecursiveCharacterTextSplitter 
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-url = "https://www.mptourism.com/destination-bhimbetka.php"
+model = ChatOpenAI(
+    model="deepseek/deepseek-chat",
+    temperature=0.7,
+    base_url=os.getenv("OPENAI_BASE_URL"),
+    api_key=os.getenv("OPENAI_API_KEY"),
 
+)
+
+prompt = PromptTemplate(
+    template='Answer the following questions \n {question} from the following text - \n {text}',
+    input_variables=['question', 'text']
+)
+
+parser = StrOutputParser()
+url = "https://www.geeksforgeeks.org/courses"
 loader = WebBaseLoader(url) 
 docs = loader.load()
-print(docs) 
+chain = prompt | model | parser 
+print(chain.invoke({'question': 'How many courses are there?', 'text': docs[0].page_content}))
